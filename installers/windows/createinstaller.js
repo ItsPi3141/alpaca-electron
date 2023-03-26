1c70fc92e93a5e7cbd77eac486b944f6401ca013
Pi <piisthebestthingintheuniverse@gmail.com>
2023-03-26 16:10:25 -0700

    1.0.4

--- installers/windows/createinstaller.js
+++ installers/windows/createinstaller.js
@@ -1,6 +1,6 @@
 const createWindowsInstaller = require("electron-winstaller").createWindowsInstaller;
 const path = require("path");
-const version = "1.0.3";
+const version = "1.0.4";
 
 getInstallerConfig()
 	.then(createWindowsInstaller)
